92e2ac32196c553bbeca1504eae240560cd573af
Рома Матвеенко <roman666matveenko@gmail.com>
2021-02-08 00:05:11 +0300

    lesson 21

--- lessons/my-app/src/components/dialogs/Dialogs.jsx
+++ lessons/my-app/src/components/dialogs/Dialogs.jsx
@@ -1,8 +1,41 @@
 import s from './Dialogs.module.css'
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <div className={s.dialogsItems} >
+               <div className={s.dialog + ' ' + s.active}>
+                   Tolik
+               </div>
+               <div className={s.dialog}>
+                   Valera
+               </div>
+               <div className={s.dialog}>
+                   Slavik
+               </div>
+               <div className={s.dialog}>
+                   Galya
+               </div>
+               <div className={s.dialog}>
+                   Tamara
+               </div>
+           </div>
+           <div className={s.messages}>
+               <div className={s.message}>
+                    I love to spy on women 
+               </div>
+               <div className={s.message}>
+                    O my gosh
+               </div>
+               <div className={s.message}>
+                    I think this is nonsence
+               </div>
+               <div className={s.message}>
+                    I was very drank tommorow
+               </div>
+               <div className={s.message}>
+                    Hello, i am Tamara
+               </div>
+           </div>
         </div>
     )
 }
