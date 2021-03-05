c2c50cbb92879dd59ba83e99e8b8dd88c0195813
Рома Матвеенко <roman666matveenko@gmail.com>
2021-03-06 01:46:05 +0300

    lesson 37, OOP

--- lessons/my-app/src/index.js
+++ lessons/my-app/src/index.js
@@ -2,23 +2,22 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import state from './redux/State'
+import store from './redux/State'
 import App from './App';
-import {addPost, updatePost, subscribe} from './redux/State'
 let rerenderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App        
-        appState={state}
-        addPost={addPost}
-        update={updatePost}
+        appState={store.getState()}
+        addPost={store.addPost.bind(store)}
+        update={store.updatePost.bind(store)}
         />
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 rerenderEntireTree();
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- lessons/my-app/src/redux/State.js
+++ lessons/my-app/src/redux/State.js
@@ -1,67 +1,65 @@
 
-let rerenderEntireTree = () => { 
-  console.log('state changed');
-}
-
-   let images = [
-    <img src="http://sun9-39.userapi.com/s/v1/ig2/8aqATIMGN_0ucbrpPT2w9-Od9s4_R-28vuF1rs263b_AnT8lBidXi9Tp1qazfob7TONkocJPt4t4cK1Z6ZOpdx3e.jpg?size=200x0&quality=96&crop=35,35,1002,1009&ava=1"/>,
-    <img src="https://cdn.igromania.ru/mnt/news/5/b/3/c/9/d/94851/e03ca27c542f8409_1920xH.jpg"/>,
-    <img src="https://vsekidki.ru/uploads/posts/2016-08/1470735121_lecdaa3axdc.jpg"/>,
-    <img src="https://cs7.pikabu.ru/post_img/2018/10/21/10/1540137624119237107.jpg"/>,
-    <img src="https://92.img.avito.st/640x480/9494408792.jpg"/>,
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBoOxv-DbMlA1aiqJ0-xnHSL8sPh8JH2rp3w&usqp=CAU"/>,
-   ]
-   
-  let state = {
-      profilePage:{
-        postData: [
-          {id: 1, text: "Hi, i'll eat Jerry" , likes: '12'},
-          {id: 2, text: 'wait!', likes: '15' },
-        ],
-        newPostText : 'aaaaaaaa'
-      },
-    
-      dialogPage: {
-        dialogData: [
-          {img: images[0] , name: "Tolik", way: "/dialogs/1"},
-          {img: images[1] , name: "Vasya", way: "/dialogs/2"},
-          {img: images[2] , name: "Masha", way: "/dialogs/3"},
-          {img: images[3] , name: "Lida", way: "/dialogs/4"},
-          {img: images[4] , name: "Stepan", way: "/dialogs/5"},
-          {img: images[5] , name: "Homer", way: "/dialogs/6"},
-        ],
-        messages: [
-          {id: 1, text: "Hello nigger" },
-          {id: 2, text: "What you say about my mom?" },
-          {id: 3, text: "Ye ye ye" },
-          {id: 4, text: "Stop. This is police!!!" },
-          {id: 5, text: "Runaway niggers!!!" },
-          {id: 6, text: "facking cops!!!" },
-        
-        ]
-      },
-        friends: [
-        {name: "Tolik", img: images[0]},
-        {name: "Ebolik", img: images[1]},
-        {name: "Andrew", img: images[2]},
-      ]
-     
-  }
-
-  window.state = state;
-  export const addPost = () => {
-   
-     state.profilePage.postData.push({id:3, text: state.profilePage.newPostText, likes: Math.floor(Math.random() * 30 )});
-     state.profilePage.newPostText = '';
-     rerenderEntireTree();
-  }
-  export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-  }
-  export const updatePost = (newText) => {
-   
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
- }
+let store = {
   
-  export default state;
+  rerenderEntireTree() {
+    console.log('state changed');
+  },
+  
+  _state: {
+   
+    profilePage: {
+      postData: [
+        { id: 1, text: "Hi, i'll eat Jerry", likes: '12' },
+        { id: 2, text: 'wait!', likes: '15' },
+      ],
+      newPostText: 'aaaaaaaa'
+    },
+
+    dialogPage: {
+      dialogData: [
+        {  name: "Tolik", way: "/dialogs/1" },
+        {  name: "Vasya", way: "/dialogs/2" },
+        {  name: "Masha", way: "/dialogs/3" },
+        {  name: "Lida", way: "/dialogs/4" },
+        {  name: "Stepan", way: "/dialogs/5" },
+        {  name: "Homer", way: "/dialogs/6" },
+      ],
+      messages: [
+        { id: 1, text: "Hello nigger" },
+        { id: 2, text: "What you say about my mom?" },
+        { id: 3, text: "Ye ye ye" },
+        { id: 4, text: "Stop. This is police!!!" },
+        { id: 5, text: "Runaway niggers!!!" },
+        { id: 6, text: "facking cops!!!" },
+
+      ]
+    },
+    friends: [
+      { name: "Tolik",   },
+      { name: "Ebolik",  },
+      { name: "Andrew",  },
+    ]
+
+  },
+  getState () {
+    return this._state
+  },
+  addPost() {
+
+    this._state.profilePage.postData.push({ id: 3, text: this._state.profilePage.newPostText, likes: Math.floor(Math.random() * 30) });
+    this._state.profilePage.newPostText = '';
+    this.rerenderEntireTree();
+  },
+  subscribe(observer) {
+    this.rerenderEntireTree = observer;
+  },
+  updatePost(newText) {
+
+    this._state.profilePage.newPostText = newText;
+    this.rerenderEntireTree();
+  },
+};
+
+  window.store = store;
+
+  export default store;
